a78b81bc2c71a5c6d61e0fff34ffb1aee54afd52
Bdubs <bmwebb9@gmail.com>
2024-01-30 08:48:46 -0500

    completed project and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # EmployeeTracker
+
+## Description
+
+This Employee Management System is a Node.js application designed to manage a company's employees using a simple command-line interface. It utilizes `mysql2` to connect to your MySQL database and `inquirer` for collecting input from the user. With this application, you can view, add, and update employees, roles, and departments within your organization.
+
+## Installation
+
+### Prerequisites
+
+- Node.js
+- MySQL
+
+### Steps
+
+1. Clone the repository:
+`  git clone [git@github.com:RJLennon/PersonaPals.git]`
+
+2.Install necessary dependencies:
+```
+npm install
+```
+3.Set up the database:
+
+Log into your MySQL shell and source the schema.sql (and optionally seeds.sql) file located in the db folder:
+
+```
+source db/schema.sql;
+source db/seeds.sql;
+```
+4. Create a .env file in the root directory and fill in your MySQL credentials:
+
+DB_NAME='employee_db'
+DB_USER='your_mysql_username'
+DB_PASSWORD='your_mysql_password'
+
+# Usage
+To start the application, run the following command in your terminal:
+
+```
+node index.js
+```
+
+Follow the interactive prompts to view, add, or update departments, roles, and employees.
+
+#Features
+View all departments, roles, and employees
+Add departments, roles, and employees
+Update employee roles
+
+#Contributing
+Contributions are welcome! Please open a pull request to contribute.
+
+## Demo
+Here is a walkthrough video [video](https://drive.google.com/file/d/1eqBevXx4D_vf-FxJqG3XZlo_omysGeZb/view?usp=sharing)
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//Add in remaining logic for the rest of the functions
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const db = require("./config/connection");
@@ -17,6 +16,9 @@ function mainMenu() {
                 'Add a Role',
                 'Add an Employee',
                 'Update an Employee Role',
+                'Delete a Department',
+                'Delete a Role',
+                'Delete an Employee',
                 'Exit'
             ]
         }
@@ -32,14 +34,26 @@ function mainMenu() {
             case 'View All Employees':
                 viewEmployees();
                 break;
+            case 'Add a Department':
+                addDepartment();
+                break;
             case 'Add a Role':
                 addRole();
                 break;
             case 'Add an Employee':
                 addEmployee();
                 break;
-            case 'Add a Department':
-                addDepartment();
+            case 'Update an Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Delete a Department':
+                deleteDepartment();
+                break;
+            case 'Delete a Role':
+                deleteRole();
+                break;
+            case 'Delete an Employee':
+                deleteEmployee();
                 break;
             case 'Exit':
                 db.end();
@@ -47,13 +61,18 @@ function mainMenu() {
         }
     })
     .catch(error => {
-        console.error(error);
+        console.error('An error occurred: ' + error);
+        mainMenu();
     });
 }
 
 function viewDepartments() {
     db.query('SELECT * FROM department', (err, results) => {
-        if (err) throw err;
+        if (err) {
+            console.error('An error occurred: ' + err);
+            mainMenu();
+            return;
+        }
         console.table(results);
         mainMenu(); 
     });
@@ -64,33 +83,34 @@ function addDepartment(){
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of the department?'
+            message: 'What is the name of the department?',
+            validate: input => input.trim() !== '' ? true : 'Department name cannot be empty.'
         }
     ])
     .then(answers => {
         db.query('INSERT INTO department SET ?', answers, (err, results) => {
-            if (err) throw err;
+            if (err) {
+                console.error('An error occurred: ' + err);
+                mainMenu();
+                return;
+            }
             console.log('Department added.');
             mainMenu();
         });
     })
     .catch(error => {
-        console.error(error);
+        console.error('An error occurred: ' + error);
+        mainMenu();
     });
-
 }
 
 function viewRoles(){
     db.query('SELECT * FROM role', (err, results) => {
-        if (err) throw err;
-        console.table(results);
-        mainMenu(); 
-    });
-}
-
-function viewEmployees(){
-    db.query('SELECT * FROM employee', (err, results) => {
-        if (err) throw err;
+        if (err) {
+            console.error('An error occurred: ' + err);
+            mainMenu();
+            return;
+        }
         console.table(results);
         mainMenu(); 
     });
@@ -101,28 +121,48 @@ function addRole(){
         {
             type: 'input',
             name: 'title',
-            message: 'What is the title of the role?'
+            message: 'What is the title of the role?',
+            validate: input => input.trim() !== '' ? true : 'Role title cannot be empty.'
         },
         {
             type: 'input',
             name: 'salary',
-            message: 'What is the salary of the role?'
+            message: 'What is the salary of the role?',
+            validate: input => !isNaN(input) && input.trim() !== '' ? true : 'Please enter a valid salary (numeric value).'
         },
         {
             type: 'input',
             name: 'department_id',
-            message: 'What is the department id of the role?'
+            message: 'What is the department id of the role?',
+            validate: input => !isNaN(input) && input.trim() !== '' ? true : 'Please enter a valid department ID (numeric value).'
         }
     ])
     .then(answers => {
         db.query('INSERT INTO role SET ?', answers, (err, results) => {
-            if (err) throw err;
+            if (err) {
+                console.error('An error occurred: ' + err);
+                mainMenu();
+                return;
+            }
             console.log('Role added.');
             mainMenu();
         });
     })
     .catch(error => {
-        console.error(error);
+        console.error('An error occurred: ' + error);
+        mainMenu();
+    });
+}
+
+function viewEmployees(){
+    db.query('SELECT * FROM employee', (err, results) => {
+        if (err) {
+            console.error('An error occurred: ' + err);
+            mainMenu();
+            return;
+        }
+        console.table(results);
+        mainMenu(); 
     });
 }
 
@@ -131,33 +171,46 @@ function addEmployee(){
         {
             type: 'input',
             name: 'first_name',
-            message: 'What is the first name of the employee?'
+            message: 'What is the first name of the employee?',
+            validate: input => input.trim() !== '' ? true : 'First name cannot be empty.'
         },
         {
             type: 'input',
             name: 'last_name',
-            message: 'What is the last name of the employee?'
+            message: 'What is the last name of the employee?',
+            validate: input => input.trim() !== '' ? true : 'Last name cannot be empty.'
         },
         {
             type: 'input',
             name: 'role_id',
-            message: 'What is the role id of the employee?'
+            message: 'What is the role id of the employee?',
+            validate: input => !isNaN(input) && input.trim() !== '' ? true : 'Please enter a valid role ID (numeric value).'
         },
         {
             type: 'input',
             name: 'manager_id',
-            message: 'What is the manager id of the employee?'
+            message: 'What is the manager id of the employee? (Enter 0 if no manager)',
+            validate: input => !isNaN(input) ? true : 'Please enter a valid manager ID (numeric value, or 0 if no manager).'
         }
     ])
     .then(answers => {
+        if (answers.manager_id === '0') {
+            answers.manager_id = null;
+        }
+        
         db.query('INSERT INTO employee SET ?', answers, (err, results) => {
-            if (err) throw err;
+            if (err) {
+                console.error('An error occurred: ' + err);
+                mainMenu();
+                return;
+            }
             console.log('Employee added.');
             mainMenu();
         });
     })
     .catch(error => {
-        console.error(error);
+        console.error('An error occurred: ' + error);
+        mainMenu();
     });
 }
 
@@ -166,23 +219,155 @@ function updateEmployeeRole(){
         {
             type: 'input',
             name: 'employee_id',
-            message: 'What is the id of the employee?'
+            message: 'What is the id of the employee?',
+            validate: input => !isNaN(input) && input.trim() !== '' ? true : 'Please enter a valid employee ID (numeric value).'
         },
         {
             type: 'input',
             name: 'role_id',
-            message: 'What is the role id of the employee?'
+            message: 'What is the new role id of the employee?',
+            validate: input => !isNaN(input) && input.trim() !== '' ? true : 'Please enter a valid new role ID (numeric value).'
         }
     ])
     .then(answers => {
         db.query('UPDATE employee SET role_id = ? WHERE id = ?', [answers.role_id, answers.employee_id], (err, results) => {
-            if (err) throw err;
+            if (err) {
+                console.error('An error occurred: ' + err);
+                mainMenu();
+                return;
+            }
             console.log('Employee role updated.');
             mainMenu();
         });
     })
     .catch(error => {
-        console.error(error);
+        console.error('An error occurred: ' + error);
+        mainMenu();
+    });
+}
+
+function deleteDepartment() {
+    db.query('SELECT * FROM department', (err, departments) => {
+        if (err) {
+            console.error('An error occurred: ' + err);
+            mainMenu();
+            return;
+        }
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'departmentId',
+                message: 'Which department do you want to delete?',
+                choices: departments.map(dept => ({ name: dept.name, value: dept.id }))
+            }
+        ])
+        .then(answer => {
+            db.query('SELECT * FROM role WHERE department_id = ?', [answer.departmentId], (err, employees) => {
+                if (err) {
+                    console.error('An error occurred: ' + err);
+                    mainMenu();
+                    return;
+                }
+
+                if (employees && employees.length > 0) {
+                    console.log('Cannot delete department because it has employees assigned to it.');
+                    mainMenu();
+                    return;
+                }
+
+                db.query('DELETE FROM department WHERE id = ?', [answer.departmentId], (err, result) => {
+                    if (err) {
+                        console.error('An error occurred: ' + err);
+                        mainMenu();
+                        return;
+                    }
+                    console.log('Department deleted.');
+                    mainMenu();
+                });
+            });
+        })
+        .catch(error => {
+            console.error('An error occurred: ' + error);
+            mainMenu();
+        });
+    });
+}
+
+
+function deleteRole() {
+    db.query('SELECT * FROM role', (err, roles) => {
+        if (err) {
+            console.error('An error occurred: ' + err);
+            mainMenu();
+            return;
+        }
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'roleId',
+                message: 'Which role do you want to delete?',
+                choices: roles.map(role => ({ name: role.title, value: role.id }))
+            }
+        ])
+        .then(answer => {
+            db.query('SELECT * FROM employee WHERE role_id = ?', answer.roleId, (err, employees) => {
+                if (employees.length > 0) {
+                    console.log('Cannot delete role because it is assigned to employees.');
+                    mainMenu();
+                    return;
+                }
+
+                db.query('DELETE FROM role WHERE id = ?', answer.roleId, (err, result) => {
+                    if (err) {
+                        console.error('An error occurred: ' + err);
+                        mainMenu();
+                        return;
+                    }
+                    console.log('Role deleted.');
+                    mainMenu();
+                });
+            });
+        })
+        .catch(error => {
+            console.error('An error occurred: ' + error);
+            mainMenu();
+        });
+    });
+}
+
+function deleteEmployee() {
+    db.query('SELECT * FROM employee', (err, employees) => {
+        if (err) {
+            console.error('An error occurred: ' + err);
+            mainMenu();
+            return;
+        }
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'employeeId',
+                message: 'Which employee do you want to delete?',
+                choices: employees.map(emp => ({ name: `${emp.first_name} ${emp.last_name}`, value: emp.id }))
+            }
+        ])
+        .then(answer => {
+            db.query('DELETE FROM employee WHERE id = ?', answer.employeeId, (err, result) => {
+                if (err) {
+                    console.error('An error occurred: ' + err);
+                    mainMenu();
+                    return;
+                }
+                console.log('Employee deleted.');
+                mainMenu();
+            });
+        })
+        .catch(error => {
+            console.error('An error occurred: ' + error);
+            mainMenu();
+        });
     });
 }
 
